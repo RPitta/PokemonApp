a324c90642ad917d65dfba47384c18db0ba64053
Rodrigo P <eskimorhcp@gmail.com>
2022-12-19 15:03:50 -0500

    modify App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import React from "react"
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ShowPokemon from './ShowPokemon';
 
-class App extends React.Component {
-  state = {
-    pokemon: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('/pokemon');
-    const body = await response.json();
-    this.setState({pokemon: body})
-  }
-
+class App extends Component {
   render() {
-    const {pokemon} = this.state;
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="App-intro">
-              <h2>Clients</h2>
-              {pokemon.map(p =>
-                <div key={p.id}>
-                    {p.name} ({p.imgSrc})
-                     </div>
-                )}
-            </div>
-          </header>
-        </div>
-    );
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/pokemon' exact={true} component={ShowPokemon}/>
+          </Switch>
+        </Router>
+    )
   }
 }
+
 export default App;
